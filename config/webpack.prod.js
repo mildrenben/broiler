4fdbb59098c4c62aee78db82fad77f7e537782a6
Ben Mildren <mildrenben@gmail.com>
2017-11-23 13:03:31 +0000

    Added code splitting and hashed module ids

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -6,15 +6,26 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-	entry: [
-		path.join(constants.SRC, 'app.js'),
-	],
+	entry: {
+		app: path.join(constants.SRC, 'app.js'),
+		// Example of code splitting out major dependencies (see line 24-26 also)
+		vendor: [
+			'react'
+		]
+	},
 	plugins: [
 		new UglifyJSPlugin({
 			sourceMap: true
 		}),
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify('production')
+		}),
+		new webpack.HashedModuleIdsPlugin(),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor'
+		}),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'runtime'
 		})
 	],
 	output: {
